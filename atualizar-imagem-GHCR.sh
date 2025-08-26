005e16e448d9c55ea73a21879d272c28059d6d00
LukaZSH <lukaalvesclaro@gmail.com>
2025-08-25 23:01:20 -0300

    Melhoria no shell script v2

--- atualizar-imagem-GHCR.sh
+++ atualizar-imagem-GHCR.sh
@@ -7,16 +7,17 @@ echo "Iniciando a atualização da aplicação ip-validator..."
 echo "Atualizar o repositório com 'git pull'..."
 git pull origin master
 
+echo "Parando os containers em execução..."
+sudo docker compose down
+
+echo "A limpar imagens Docker antigas..."
+sudo docker image prune -f
 
 echo "Puxando a imagem mais recente do GHCR..."
 sudo docker-compose pull
 
 echo "Subindo os containers com a nova versão..."
 sudo docker-compose up -d
-
-
-echo "A limpar imagens Docker antigas..."
-sudo docker image prune -f
 
 echo "Aplicação atualizada com sucesso!"
 echo "Acesse: http://192.168.3.2"
