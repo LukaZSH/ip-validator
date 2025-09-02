dfcf2b86c614638dcd4fdd5776eb0c26c1c869eb
LukaZSH <lukaalvesclaro@gmail.com>
2025-09-02 10:12:54 -0300

    Correção no Docker Compose

--- scripts/atualizar-imagem-GHCR.sh
+++ scripts/atualizar-imagem-GHCR.sh
@@ -15,7 +15,7 @@ echo "Puxando a imagem mais recente do GHCR..."
 sudo docker-compose pull
 
 echo "Subindo os contêineres com a nova versão..."
-sudo docker-compose up --build --no-cache -d
+sudo docker-compose up -d
 
 echo ""
 echo "Aplicação atualizada com sucesso!"
